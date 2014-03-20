03f802beda34ddc6e81933a14e630de3d987ffc7
patrick van lohuizen <git.patrickvanlohuizen@outlook.com>
2014-03-20 23:00:57 +0100

    Added testproject, reduced the number of unions to 10

---
+++ DiscriminatedUnions/DiscriminatedUnions.Test/Examples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiscriminatedUnions.Test
+{
+    [TestClass]
+    public class Examples
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
