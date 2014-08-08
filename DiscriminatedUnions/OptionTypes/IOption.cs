1f8940b44295630abeee04dbbe4c0702b943f7de
Patrickkk <patrickvanlohuizen@outlook.com>
2014-08-08 13:06:18 +0200

    Added option types.

---
+++ DiscriminatedUnions/OptionTypes/IOption.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OptionTypes
+{
+    public interface IOption<T>
+    {
+        void Match(Action<T> withValue, Action withoutValue);
+
+        void IfNotNullDo(Action<T> withValue);
+
+        TResult Match<TResult>(Func<T, TResult> withValue, Func<TResult> withoutValue);
+
+        TResult Match<TResult>(Func<T, TResult> withValue, TResult withoutValue);
+    }
+
+    public static class OptionExtensions
+    {
+        public static IOption<T> ToOption<T>(this T value)
+        {
+            if (value == null)
+            {
+                return new None<T>();
+            }
+            else
+            {
+                return new Some<T>(value);
+            }
+        }
+    }
+}
---
+++ DiscriminatedUnions/OptionTypes/None.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OptionTypes
+{
+    [Serializable]
+    public class None<T> : IOption<T>
+    {
+        public void IfNotNullDo(Action<T> withValue)
+        {
+
+        }
+
+        public void Match(Action<T> withValue, Action withoutValue)
+        {
+            withoutValue();
+        }
+
+        public TResult Match<TResult>(Func<T, TResult> withValue, TResult withoutValue)
+        {
+            return withoutValue;
+        }
+
+        public TResult Match<TResult>(Func<T, TResult> withValue, Func<TResult> withoutValue)
+        {
+            return withoutValue();
+        }
+
+        public override string ToString()
+        {
+            return "null value for " + typeof(T).Name;
+        }
+    }
+}
