a33bfef3adce5e1c9f316217c6481f5899c27481
Patrickkk <patrickvanlohuizen@outlook.com>
2014-08-08 15:37:36 +0200

    updated old discriminated union package.

--- FunctionalSharp/.nuget/PackageDiscriminatedUnions.bat
+++ FunctionalSharp/.nuget/PackageDiscriminatedUnions.bat
@@ -2,8 +2,6 @@ mode con:cols=140 lines=2500
 
 nuget.exe update -self
 
-CALL Build.bat
-
 mkdir Publish
 NuGet Pack DiscriminatedUnions.nuspec -OutputDirectory Publish
 Nuget push "\Publish\DiscriminatedUnions*.nupkg"
