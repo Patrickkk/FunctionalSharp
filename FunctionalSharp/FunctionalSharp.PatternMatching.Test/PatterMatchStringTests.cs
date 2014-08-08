ede22392b9be11e619ee5399ac1b6d02047a6178
Patrickkk <patrickvanlohuizen@outlook.com>
2014-08-08 13:33:13 +0200

    Added more testprojects and patternmatching tests.

---
+++ FunctionalSharp/FunctionalSharp.PatternMatching.Test/PatterMatchStringTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionalSharp.PatternMatching.Test
+{
+    [TestClass]
+    public class PatterMatchStringTests
+    {
+        [TestMethod]
+        public void TestMatchFirst()
+        {
+            var result = "AAA".Match()
+                .With(s => s.Contains("B"), false)
+                .With(s => s.Contains("A"), true)
+                .Else(false);
+        }
+
+        [TestMethod]
+        public void TestMatchingSecondString()
+        {
+            var result = "ABA".Match()
+                .With(s => s.Contains("B"), "B")
+                .With(s => s.Contains("A"), "A")
+                .Else("C");
+
+            Assert.AreEqual("B", result);
+        }
+
+        [TestMethod]
+        public void TestMatchingThirdString()
+        {
+            var result = "AAA".Match()
+                .With(s => s.Contains("B"), "B")
+                .With(s => s.Contains("A"), "A")
+                .Else("C");
+
+            Assert.AreEqual("A", result);
+        }
+
+        [TestMethod]
+        public void TestMatchingElse()
+        {
+            var result = "XXX".Match()
+                .With(s => s.Contains("B"), "B")
+                .With(s => s.Contains("A"), "A")
+                .Else("C");
+
+            Assert.AreEqual("C", result);
+        }
+    }
+}
