7dff76a8a9dc0e75017c5f053a8ed429c26c2113
Patrickkk <patrickvanlohuizen@outlook.com>
2015-04-22 16:40:29 +0200

    added pattermatching with actions instead of functions

---
+++ FunctionalSharp/FunctionalSharp.PatternMatching.Test/PatternMatchActionSingleTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FunctionalSharp.PatternMatching.Test
+{
+    [TestClass]
+    public class PatternMatchActionSingleTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            "TEST".Match()
+                .With("", () => { })
+                .ElseNothing();
+        }
+
+        [TestMethod]
+        public void TODO()
+        {
+            // TODO more tests.
+        }
+    }
+}
