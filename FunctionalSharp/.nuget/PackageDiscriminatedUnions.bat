4e0ba8a2ae52f6b1a84f5439b003d704fbc91521
Patrickkk <patrickvanlohuizen@outlook.com>
2015-11-06 13:57:17 +0100

    use correct nuget package for discriminatedunions

--- FunctionalSharp/.nuget/PackageDiscriminatedUnions.bat
+++
@@ -1,9 +0,0 @@
-mode con:cols=140 lines=2500
-
-nuget.exe update -self
-
-mkdir Publish
-NuGet Pack DiscriminatedUnions.nuspec -OutputDirectory Publish
-Nuget push "\Publish\DiscriminatedUnions*.nupkg"
-rmdir Publish /s /q
-pause
